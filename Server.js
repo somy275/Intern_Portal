6f638bfc1b14ed5a15bc71b0f3d4db7d5346221c
somy275 <somyranjank@gmail.com>
2025-08-04 14:58:24 +0530

    some changes

--- Server.js
+++ Server.js
@@ -8,12 +8,7 @@ import path from "path";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-type", "Authorization"],
-    credentials: true
-}))
+app.use(cors())
 app.use(express.json());
 const internData = {
     name: 'Sahil Kumar',
